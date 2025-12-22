9fdef3a23375a22560bd90b0917e9a45f8a90a7c
Chris Allen <burlingtonbertie99@gmail.com>
2025-12-22 11:17:27 +0000

    Deployed cli for conversion to PlantUML++

---
+++ structurizr-cli/convertturizr.bat
@@ -0,0 +1,6 @@
+REM @echo off
+REM setlocal
+REM set SCRIPT_DIR=%~dp0
+REM java -cp "%SCRIPT_DIR%;%SCRIPT_DIR%\lib\*;" com.structurizr.cli.StructurizrCliApplication %*
+copy ..\workspace.dsl .
+structurizr.bat  export -workspace workspace.dsl  -format plantuml -output out\
---
+++ structurizr-cli/converturizr.bat
@@ -0,0 +1,2 @@
+copy ..\workspace.dsl .
+structurizr.bat  export -workspace workspace.dsl  -format plantuml -output out\
